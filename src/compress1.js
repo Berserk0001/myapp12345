ac4c5a443a651ec72e98a274f1541f7b97a761c9
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-06 16:37:24 +0530

    Create compress1.js

---
+++ src/compress1.js
@@ -0,0 +1,49 @@
+/*
+ * compress.js
+ * A module that compresses an image.
+ * compress(httpRequest, httpResponse, ReadableStream);
+ */
+import sharp from 'sharp';
+import redirect from './redirect.js';
+sharp.cache(false);
+sharp.concurrency(1);
+const sharpStream = () => sharp({ unlimited: true });
+
+ function compress(req, res, input) {
+  const format = 'jpeg'
+
+  /*
+   * Determine the uncompressed image size when there's no content-length header.
+   */
+
+  /*
+   * input.pipe => sharp (The compressor) => Send to httpResponse
+   * The following headers:
+   * |  Header Name  |            Description            |           Value            |
+   * |---------------|-----------------------------------|----------------------------|
+   * |x-original-size|Original photo size                |OriginSize                  |
+   * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
+   */
+  input.pipe(sharpStream()
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true
+    })
+    .toBuffer((err, output, info) => _sendResponse(err, output, info, format, req, res)))
+}
+
+function _sendResponse(err, output, info, format, req, res) {
+  if (err || !info) return redirect(req, res);
+
+  res.setHeader('content-type', 'image/' + format);
+  res.setHeader('content-length', info.size);
+  res.setHeader('x-original-size', req.params.originSize);
+  res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+  res.status(200);
+  res.write(output);
+  res.end();
+}
+
+export default compress;
