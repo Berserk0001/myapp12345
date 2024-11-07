c73adaf7f2370492a8f690e2a488ccc5ecb36425
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-07 15:52:19 +0530

    Update proxy2.js

--- src/proxy2.js
+++ src/proxy2.js
@@ -8,33 +8,30 @@ import _ from "lodash";
 import got from "got";
 import shouldCompress from "./shouldCompress.js";
 import redirect from "./redirect.js";
-import compress from "./compress1.js";
+import compress from "./compress.js";
 import copyHeaders from "./copyHeaders.js";
 const { pick } = _;
 
 async function proxy(req, res) {
-
   try {
     // Fetch the image as a stream using `got`
-   let responseStream = await got.stream(req.params.url, {
+    let responseStream = await got.stream(req.params.url, {
       headers: {
         ...pick(req.headers, ["dnt"]),
         "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
         "x-forwarded-for": req.socket.localAddress,
         via: "1.1 2e9b3ee4d534903f433e1ed8ea30e57a.cloudfront.net (CloudFront)",
       },
-    decompress: false,
+      decompress: false,
       maxRedirects: 4, // Handles redirections
       throwHttpErrors: false // Do not throw errors for non-2xx responses
     });
 
-
-
     // Handle the response before streaming
-    responseStream.on('response', (httpResponse) => {
+    responseStream.once('response', (httpResponse) => {
       if (httpResponse.statusCode !== 200) {
         // If the response status is not 200, redirect the client
-       return redirect(req, res);
+        return redirect(req, res);
       }
 
       // Copy headers and set necessary response headers
@@ -57,7 +54,8 @@ async function proxy(req, res) {
         responseStream.pipe(res);
       }
     });
-        // Handle stream errors by attaching the error handler upfront
+
+    // Handle stream errors by attaching the error handler upfront
     responseStream.on('error', () => req.socket.destroy());
 
   } catch (err) {
