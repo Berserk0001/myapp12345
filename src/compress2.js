f13c695febffa21c77bc5f643bf05f902cf9f077
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-08 19:19:04 +0530

    Update compress2.js

--- src/compress2.js
+++ src/compress2.js
@@ -26,7 +26,7 @@ function compress(req, res, input) {
    * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
    */
 
-  input.pipe(sharpStream()
+  input.body.pipe(sharpStream()
     .grayscale(req.params.grayscale)
     .toFormat(format, {
       quality: req.params.quality
