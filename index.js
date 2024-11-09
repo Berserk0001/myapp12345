29965f914d90e9eab3ddd7abaf3628ffc8085d4b
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-09 16:44:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 // Uncomment the next line if you want to trust the proxy
 // app.enable('trust proxy');
+app.disable('x-powered-by');
 
 app.get('/', params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
