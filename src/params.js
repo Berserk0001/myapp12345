538aeac6d4f29ed91fbb58d9c37795629e97342e
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-11 21:28:20 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -9,8 +9,6 @@ function params(req, res, next) {
   req.params.webp = !req.query.jpeg;
   req.params.grayscale = req.query.bw != 0;
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
-  req.params.imgWidth = parseInt(req.query.width, 10); // Extract imgWidth
-  req.params.imgHeight = parseInt(req.query.height, 10); // Extract imgHeight
 
   next();
 }
