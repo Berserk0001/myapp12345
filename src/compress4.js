9b5c208caff77e52c083be86f2b3c2c05f4d8584
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-11 12:49:21 +0530

    Update compress4.js

--- src/compress4.js
+++ src/compress4.js
@@ -8,41 +8,46 @@ import redirect from './redirect.js';
 sharp.cache(false);
 sharp.concurrency(1);
 
-const sharpStream = (format, quality, resizeOptions) => {
-  let pipeline = sharp({ unlimited: true }).toFormat(format, { quality });
-  if (resizeOptions) {
-    pipeline = pipeline.resize(resizeOptions);
-  }
-  return pipeline;
-};
+const sharpStream = () => sharp({ unlimited: true });
 
 function compress(req, res, input) {
-  let format = 'jpeg';
-  let compressionQuality = req.params.quality || 80;
-  let resizeOptions = null;
+  let format = 'webp';
+  let compressionQuality = req.params.quality;
   const imgWidth = req.params.imgWidth;
   const imgHeight = req.params.imgHeight;
+  let resizeWidth = null;
+  let resizeHeight = null;
+  let effortCPU = 1;
 
-  // Adjust format, compression quality, and resizing based on image dimensions
-  if (imgHeight > 12480) { // Longstrip image
+  if (imgHeight > 12480) {
+    // damn longstrip image
     format = 'webp';
     compressionQuality *= 0.5;
-    resizeOptions = { height: 12480 };
+    resizeHeight = 12480;
+    effortCPU = 6;
   } else if (imgWidth > 1280 && imgHeight < 9360) {
     format = 'webp';
     compressionQuality *= 0.5;
-    resizeOptions = { width: 960 };
+    resizeWidth = 960;
+    effortCPU = 6;
   } else if (imgWidth > 960 && imgHeight < 2880) {
     format = 'webp';
     compressionQuality *= 0.5;
-    resizeOptions = { width: 864 };
+    resizeWidth = 864;
+    effortCPU = 6;
   } else {
     format = 'webp';
     compressionQuality *= 0.5;
+    effortCPU = 6;
   }
 
-  input.pipe(sharpStream(format, compressionQuality, resizeOptions)
+  input.pipe(sharpStream()
+    .resize(resizeWidth, resizeHeight)
     .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: compressionQuality,
+      effort: effortCPU
+    })
     .on('error', (err) => {
       console.error('Sharp error:', err.message || err);
       return redirect(req, res);
