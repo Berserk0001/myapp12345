9b8e2514f0d6e27dd0efbef8b773b7ab44ee533a
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-11 12:49:56 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,16 +1,18 @@
 "use strict";
-const DEFAULT_QUALITY = 40
+const DEFAULT_QUALITY = 40;
 
 function params(req, res, next) {
   let url = req.query.url;
   if (!url) return res.send('bandwidth-hero-proxy');
 
   req.params.url = decodeURIComponent(url);
-  req.params.webp = !req.query.jpeg
-  req.params.grayscale = req.query.bw != 0
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+  req.params.webp = !req.query.jpeg;
+  req.params.grayscale = req.query.bw != 0;
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
+  req.params.imgWidth = parseInt(req.query.width, 10); // Extract imgWidth
+  req.params.imgHeight = parseInt(req.query.height, 10); // Extract imgHeight
 
-  next()
+  next();
 }
 
-export default params;
+module.exports = params;
