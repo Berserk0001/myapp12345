ee777a1ec524df60e6514a207194f13b399f328d
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-12 10:16:03 +0530

    Update compress2.js

--- src/compress2.js
+++ src/compress2.js
@@ -27,7 +27,7 @@ function compress(req, res, input) {
    */
 
   input.body.pipe(sharp({ unlimited: true })
-    .resize(null, 16383, {
+    .resize(null, 800, {
         withoutEnlargement: true
       })
     .grayscale(req.params.grayscale)
