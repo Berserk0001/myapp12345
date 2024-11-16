673d6155716a5f21edb3a371af461d1d4fd184ba
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-16 18:27:54 +0530

    Update compress4.js

--- src/compress4.js
+++ src/compress4.js
@@ -13,8 +13,12 @@ sharp.cache(false); // Disable cache
 sharp.simd(true); // Enable SIMD (Single Instruction, Multiple Data)
 sharp.concurrency(availableParallelism()); // Set concurrency based on system resources
 
-const sharpStream = () => sharp({ animated: false, unlimited: true });
-
+const sharpStream = () => sharp({
+  animated: false, // Optional: if animated images should be handled
+  unlimited: true, // Allows large images without restrictions
+  failOn: 'none', // Do not fail on invalid images
+  limitInputPixels: false, // Disable input pixel size limitation
+});
 export default function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg';
 
