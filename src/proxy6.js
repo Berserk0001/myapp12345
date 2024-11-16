0427cd1b8710cd4fda0a5d15e0af52be7943fe5e
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-16 16:49:17 +0530

    Update proxy6.js

--- src/proxy6.js
+++ src/proxy6.js
@@ -7,56 +7,54 @@ import compress from "./compress1.js";
 import copyHeaders from "./copyHeaders.js";
 const { pick } = _;
 
-async function proxy(req, res) {
-  
+function proxy(req, res) {
+  let userAgent = randomDesktopUA();
 
-  try {
-   let userAgent = randomDesktopUA();
-    let response =await axios.get(req.params.url, {
-      headers: {
-        ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
-        "user-agent": userAgent,
-        "x-forwarded-for": req.socket.localAddress,
-        via: "1.1 2e9b3ee4d534903f433e1ed8ea30e57a.cloudfront.net (CloudFront)",
-      },
-      responseType: "stream",
-      timeout: 10000,
-      decompress: false,
-      maxRedirects: 4,
-    });
-
-    if (response.status !== 200) {
-     // throw new Error(`Unexpected response status: ${response.status}`);
-      redirect(req, res);
-    }
-
-    copyHeaders(response, res);
-    res.setHeader("content-encoding", "identity");
-    req.params.originType = response.headers["content-type"] || "";
-    req.params.originSize = parseInt(response.headers["content-length"], 10) || 0;
-
-    // Handle streaming response
-    response.data.on('error', () => req.socket.destroy());
-
-    if (shouldCompress(req)) {
-      return compress(req, res, response);
-    } else {
-      // Bypass compression
-      res.setHeader("x-proxy-bypass", 1);
-
-      // Set specific headers
-      for (const headerName of ["accept-ranges", "content-type", "content-length", "content-range"]) {
-        if (headerName in response.headers) res.setHeader(headerName, response.headers[headerName]);
+  axios.get(req.params.url, {
+    headers: {
+      ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
+      "user-agent": userAgent,
+      "x-forwarded-for": req.socket.localAddress,
+      via: "1.1 2e9b3ee4d534903f433e1ed8ea30e57a.cloudfront.net (CloudFront)",
+    },
+    responseType: "stream",
+    timeout: 10000,
+    decompress: false,
+    maxRedirects: 4,
+  })
+    .then(response => {
+      if (response.status !== 200) {
+        // If the status is not 200, handle the redirect
+        return redirect(req, res);
       }
 
-      return response.data.pipe(res);
-    }
-    }
-   catch (err) {
-  //  console.error("Proxy error:", err.message || err);
-    redirect(req, res);
-  }
-}
+      copyHeaders(response, res);
+      res.setHeader("content-encoding", "identity");
+      req.params.originType = response.headers["content-type"] || "";
+      req.params.originSize = parseInt(response.headers["content-length"], 10) || 0;
 
+      // Handle streaming response
+      response.data.on('error', () => req.socket.destroy());
+
+      if (shouldCompress(req)) {
+        return compress(req, res, response);
+      } else {
+        // Bypass compression
+        res.setHeader("x-proxy-bypass", 1);
+
+        // Set specific headers
+        for (const headerName of ["accept-ranges", "content-type", "content-length", "content-range"]) {
+          if (headerName in response.headers) res.setHeader(headerName, response.headers[headerName]);
+        }
+
+        return response.data.pipe(res);
+      }
+    })
+    .catch(err => {
+      // Log error if needed, otherwise redirect on failure
+      console.error("Proxy error:", err.message || err);
+      redirect(req, res);
+    });
+}
 
 export default proxy;
